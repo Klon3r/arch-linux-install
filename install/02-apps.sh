c48495cb198b5ab50f6139d9d89d8f74dbd64a42
Klon3r <117739313+Klon3r@users.noreply.github.com>
2025-07-06 22:05:40 +1000

    Fixed spelling bug

--- install/02-apps.sh
+++ install/02-apps.sh
@@ -1,5 +1,5 @@
 # Install Apps
-sudo pacman -S --noconfirm neovim zip unzip git wl-clipboard xclip cliphist nwg-look obsidian steam gnome-browser-connector nwg-look ttf-melso-nerd ttf-jetbrains-mono-nerd  
+sudo pacman -S --noconfirm neovim zip unzip git wl-clipboard xclip cliphist nwg-look obsidian steam gnome-browser-connector nwg-look ttf-meslo-nerd ttf-jetbrains-mono-nerd  
 yay -S --noconfirm gdm-settings visual-studio-code-bin ttf-ms-win11-auto
 
 echo -e "\nInstalled: Apps"
