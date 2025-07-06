79df99b6a48dc50af69de9f07cba2b03cc5ee2ef
Klon3r <117739313+Klon3r@users.noreply.github.com>
2025-07-06 22:59:26 +1000

    Updated install script

--- install.sh
+++ install.sh
@@ -2,11 +2,7 @@
 set -e
 
 # Install everything in /install
-
 for script in ./install/*.sh; do
   echo "Running $script..."
   bash "$script"
 done
-
-# OhMyZsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
--- install/11-shell.sh
+++ install/11-shell.sh
@@ -1,6 +1,9 @@
 # Install zsh
 sudo pacman -S --noconfirm zsh
 
+# OhMyZsh
+RUNZSH=no CHSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # PowerLevel10k
 yay -S --noconfirm zsh-theme-powerlevel10k-git
 echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
