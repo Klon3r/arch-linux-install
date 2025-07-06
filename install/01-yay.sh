843ee859eadda5a9090f456860674f4aed27aed2
Klon3r <117739313+Klon3r@users.noreply.github.com>
2025-07-06 23:01:07 +1000

    Added --noconfirm

--- install/01-yay.sh
+++ install/01-yay.sh
@@ -1,5 +1,5 @@
 # Install Yay
-sudo pacman -S go
+sudo pacman -S --noconfirm go
 sudo pacman -S --noconfirm --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
 echo -e "\nInstalled: Yay"
