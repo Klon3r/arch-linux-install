368a90f788fae0e0ba8e0dfab02e71c2196b7941
Klon3r <117739313+Klon3r@users.noreply.github.com>
2025-07-07 22:29:10 +1000

    Removed git

--- install/02-apps.sh
+++ install/02-apps.sh
@@ -1,5 +1,5 @@
 # Install Apps
-sudo pacman -S --noconfirm zip neovim unzip git wl-clipboard xclip cliphist nwg-look obsidian gnome-browser-connector nwg-look ttf-meslo-nerd ttf-jetbrains-mono-nerd  
+sudo pacman -S --noconfirm zip neovim unzip wl-clipboard xclip cliphist nwg-look obsidian gnome-browser-connector nwg-look ttf-meslo-nerd ttf-jetbrains-mono-nerd  
 yay -S --noconfirm gdm-settings visual-studio-code-bin ttf-ms-win11-auto
 
 echo -e "\nInstalled: Apps"
