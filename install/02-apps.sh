52fd102b0da5bfe11ae2b48d1d851ac30d79f414
Keiran <117739313+Klon3r@users.noreply.github.com>
2025-07-17 16:43:36 +1000

    Update 02-apps.sh

--- install/02-apps.sh
+++ install/02-apps.sh
@@ -1,5 +1,5 @@
 # Install Apps
 sudo pacman -S --noconfirm zip neovim unzip wl-clipboard xclip cliphist nwg-look obsidian gnome-browser-connector nwg-look ttf-meslo-nerd ttf-jetbrains-mono-nerd
-yay -S --noconfirm gdm-settings visual-studio-code-bin brave-bin
+yay -S --noconfirm gdm-settings visual-studio-code-bin
 
 echo -e "\nInstalled: Apps"
