aba06cd5649ddb3d61406c3bbbb8d0615663c6ca
Klon3r <117739313+Klon3r@users.noreply.github.com>
2025-08-09 17:56:57 +1000

    Fixed thunar.sh

--- install/09-thunar.sh
+++ install/09-thunar.sh
@@ -1,7 +1,19 @@
-# Remove/Add settings.ini
-sudo rm /usr/share/gtk-3.0/settings.ini
-sudo cp config/thunar/settings.ini ~/usr/share/gtk-3.0/
+# Install Thunar fixes/settings
 
-sudo cp config/thunar/helpers.rc ~/.config/xfce4/helpers.rc
+CONFIG_DIR="/home/$USER/git/arch-linux-install/config/thunar"
+
+# Settings.ini
+if [ -f "$CONFIG_DIR/settings.ini" ]; then
+    sudo cp "$CONFIG_DIR/settings.ini" /usr/share/gtk-3.0/
+else
+    echo "Warning: $CONFIG_DIR/settings.ini not found."
+fi
+
+# helpers.rc
+if [ -f "$CONFIG_DIR/helpers.rc" ]; then
+    sudo cp "$CONFIG_DIR/helpers.rc" ~/.config/xfce4/helpers.rc
+else
+    echo "Warning: $CONFIG_DIR/helpers.rc not found."
+fi
 
 echo -e "\nInstalled: Thunar Settings"
